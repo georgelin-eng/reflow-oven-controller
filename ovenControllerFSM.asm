7e97de4ec4b1dffe80481d1737ce38c481524006
RohanKami <rohankamineni@gmail.com>
2024-02-06 14:40:27 -0800

    Quick button logic

---
+++ ovenControllerFSM.asm
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+
+
+;Button nested logic -> we should have buttons constantly checking in the main loop for a stop (i.e the stop should be instantaneous)
+        ;->Buttons should allow for adjustment of soak temp, soak time, reflow temp, reflow time (Ui should be designed to make all these visible and clear)
+        ;->Start button should either be used only for start or used for start/stop (we could also maybe implement a pause button)
